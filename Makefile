6d7a257a26ecb56c7aeacca9a0a24327e2d5063a
Craig Hesling <craig@hesling.com>
2012-12-02 03:18:07 -0500

    Almost finished

---
+++ Makefile
@@ -0,0 +1,26 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -pedantic
+LDFLAGS = 
+
+BIN_PATH = $(HOME)/bin
+
+.PHONY: all clean install uninstall
+
+all: bytes2csv
+
+bytes2csv: bytes2csv.o
+
+bytes2csv.o: main.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -c -o bytes2csv.o main.c
+
+clean:
+	$(RM) bytes2csv bytes2csv.o
+
+install:
+	[ -d $(BIN_PATH) ] || { echo "### Creating local bin ($(HOME)/bin)"; mkdir -p $(BIN_PATH); }
+	[ ! -e $(BIN_PATH)/bytes2csv ] || { echo "### Removing existing file"; rm -i $(BIN_PATH)/bytes2csv; }
+	cp bytes2csv $(BIN_PATH)/
+	which bytes2csv >/dev/null || echo "!!! $(HOME)/bin not in PATH"
+
+uninstall:
+	rm -i $(BIN_PATH)/bytes2csv
---
+++ main.c
@@ -0,0 +1,94 @@
+/**
+ * Reads a binary file and outputs a Comma Seperated Value(CSV) 
+ * 	list of it's chars represented in different ways.
+ *
+ * @author Craig Hesling
+ * @date Dec 2, 2012
+ */
+
+#include <unistd.h> /* getopt */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h> /* strerr() */
+
+#define TRUE 1
+#define FALSE 0
+
+void usage(FILE *out)
+{
+	fprintf(out, "Usage: bytes2csv [-a] [-n] [-h] [input]\n");
+	fprintf(out, "\t-a: insert row of ascii\n");
+	fprintf(out, "\t-n: insert row of numbers\n");
+	fprintf(out, "\t-h: help\n");
+	fprintf(out, "\t-h: help\n");
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+/* int getopt(int argc, char * const argv[], const char *optstring); */
+
+int main(int argc, char *argv[]) {
+	int ascii = FALSE, numbs = FALSE;
+	char *cmdin = NULL;
+	int opt = 0;
+	FILE *input = NULL;
+
+	while((opt = getopt(argc, argv, "anh")) != -1){
+		switch (opt){
+			case 'a':
+				ascii = TRUE;
+				break;
+			case 'n':
+				numbs = TRUE;
+				break;
+			case '1':
+				cmdin = optarg;
+				break;
+			case 'h':
+			case '?':
+				usage(stderr);
+				exit(1);
+				break;
+		}
+	}
+
+	/* is input file an argument or piped in */
+	if(cmdin)
+	{
+		input = fopen(optarg,"r");
+		if(!input){
+			fprintf(stderr, "Error opening %s - %s\n", optarg, strerror(errno));
+			exit(1);
+		}
+	}
+	else
+		input = stdin;
+
+	/* print hex chars row 1 */
+	printf("%X", fgetc(input));
+	while(!feof(input))
+		printf(",%X", fgetc(input));
+
+	/* print ascii chars row if -a flag */
+	if(ascii)
+	{
+		rewind(input);
+		printf("%c", fgetc(input));
+		while(!feof(input))
+			printf(",%c", fgetc(input));
+	}
+	/* print numbs row if -n flag */
+	if(numbs)
+	{
+		rewind(input);
+		printf("%u", fgetc(input));
+		while(!feof(input))
+			printf(",%u", fgetc(input));
+	}
+
+	if(input != stdout)
+		fclose(input);
+
+	return 0;
+}
